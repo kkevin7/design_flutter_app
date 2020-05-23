56c5e176cecece9a6ce83dbc7c82d10a951de5b0
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-05-22 21:29:14 -0600

    Adding titles

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -9,6 +9,13 @@ class BotonesPage extends StatelessWidget {
         body: Stack(
       children: <Widget>[
         _fondoApp(),
+        SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              _titulos(),
+            ],
+          ),
+        ),
       ],
     ));
   }
@@ -27,30 +34,42 @@ class BotonesPage extends StatelessWidget {
           ])),
     );
 
-  final cajaRosa = Transform.rotate(
-    angle: -pi/5.0,
-    child: Container(
-    height: 360.0,
-    width: 360.0,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(80.0),
-      gradient: LinearGradient(
-        colors: [
-          Color.fromRGBO(236,98,188,1.0),
-          Color.fromRGBO(241,142,172,1.0),
-        ]
+    final cajaRosa = Transform.rotate(
+      angle: -pi / 5.0,
+      child: Container(
+        height: 360.0,
+        width: 360.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(80.0),
+          gradient: LinearGradient(colors: [
+            Color.fromRGBO(236, 98, 188, 1.0),
+            Color.fromRGBO(241, 142, 172, 1.0),
+          ]),
+        ),
       ),
-    ),
-  ),
-  );
+    );
 
     return Stack(
       children: <Widget>[
         gradiente,
-        Positioned(
-          top: -100,
-          child: cajaRosa),
+        Positioned(top: -100, child: cajaRosa),
       ],
+    );
+  }
+
+  Widget _titulos() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Classify Transaction', style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold),),
+            SizedBox(height: 10.0,),
+            Text('Classify thistrasaction into a particular category', style: TextStyle(color: Colors.white, fontSize: 18.0, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
     );
   }
 }
